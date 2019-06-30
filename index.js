7309c91570179fb8421433b9efb52cb72fd7ce2f
Memento-mo <gpologov@gmail.com>
2019-06-30 16:15:08 +0300

    f

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const path = require("path");
 require('./models/Task');
 
 mongoose.connect(keys.mongoURL, { useNewUrlParser: true });
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(bodyParser.json());
 require('./routes/taskRouter')(app);
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Сервер запущен"));
