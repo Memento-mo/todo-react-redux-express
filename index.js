c80e09d57b430ad1489763a503db07502a6435a8
Memento-mo <gpologov@gmail.com>
2019-06-30 13:06:15 +0300

    fix

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const path = require("path");
 require('./models/Task');
 
 mongoose.connect(keys.mongoURL, { useNewUrlParser: true });
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(bodyParser.json());
 require('./routes/taskRouter')(app);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
